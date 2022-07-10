51769ba3b1c3d4e0a53db4bc9315d6800b48a69b
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-10 10:55:16 -0700

    Task 1 - print d

--- pr_int.c
+++ pr_int.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
+ * Print an integer
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
+
 int print_int(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int int_input;
